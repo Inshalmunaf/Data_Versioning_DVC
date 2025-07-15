fe9b9a9fd2dd371426d6380dd51e0978a9b6a7c1
Inshalmunaf <inshalmunaf@gmail.com>
2025-07-15 16:28:41 +0500

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+data ={
+    "Name" : ["Inshal" , "Zain" ,"Ahmed"],
+    "Age" :[22,24,27],
+    "Address" : ['abc' , 'xyz' , 'qwr']
+}
+
+df = pd.DataFrame(data)
+
+datadir= 'data'
+os.makedirs(datadir ,exist_ok=True)
+
+file_path=os.path.join(datadir , "sample.csv")
+
+df.to_csv(file_path , index=False)
+
+print('Csv file saved to ',file_path)
